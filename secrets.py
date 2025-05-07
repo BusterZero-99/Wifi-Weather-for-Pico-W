d47241bdbc9c9c8063e1a87904d5696418ab8533
BusterZero99 <desilvalaurence+github@gmail.com>
2025-05-07 18:27:10 +0100

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,9 @@
+secrets = {
+    'ssid': 'wifiname',
+    'pw': 'wifipassword',
+    'ntptimehost': 'time.windows.com',
+    'weather_key': 'bd5e378503939ddaee76f12ad7a97608',
+    'city1': 'London',
+    'city2': 'Ware',
+    'city3': 'Atlanta'
+}
