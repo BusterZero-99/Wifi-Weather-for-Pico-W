4360f06bf1cc7da4a65bb995802257c15a9b61a6
BusterZero99 <desilvalaurence+github@gmail.com>
2025-05-07 19:17:44 +0100

    Added timezonedb key

--- secrets.py
+++ secrets.py
@@ -3,6 +3,7 @@ secrets = {
     'pw': 'wifipassword',
     'ntptimehost': 'time.windows.com',
     'weather_key': 'bd5e378503939ddaee76f12ad7a97608',
+    'timedb_key': '8915EX4YX6Z2',
     'city1': 'London',
     'city2': 'Ware',
     'city3': 'Atlanta'
