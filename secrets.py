7629843143c29310316dc0926f5c9be542bc6270
BusterZero99 <desilvalaurence+github@gmail.com>
2025-05-08 10:52:49 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,8 +2,8 @@ secrets = {
     'ssid': 'wifiname',
     'pw': 'wifipassword',
     'ntptimehost': 'time.windows.com',
-    'weather_key': 'bd5e378503939ddaee76f12ad7a97608',
-    'timedb_key': '8915EX4YX6Z2',
+    'weather_key': 'bd5e378503939ddaee76f12ad7a97608', # Keep the same. DO NOT CHANGE!
+    'timedb_key': '8915EX4YX6Z2',                      # Keep the same. DO NOT CHANGE!
     'city1': 'London',
     'city2': 'Ware',
     'city3': 'Atlanta'
